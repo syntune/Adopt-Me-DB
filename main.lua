cd868502309629d63c25adadc31e2e7d190980eb
bred <89583473+syntune@users.noreply.github.com>
2023-08-04 23:50:50 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,30 +3,30 @@ local hs = game:GetService("HttpService");
 
 
 local function moduleharvest(module)
-	local newstuff = {}
+	local result = {}
 	for i,v in pairs(module) do
 		if type(v) == "table" then
 			for i2, v2 in pairs(v) do 
-				local yourmom = {}
+				local attributes = {}
 				for i3,v3 in pairs(v2) do 
-					yourmom[i3] = v3 
+					attributes[i3] = v3 
 				end 
-				table.insert(newstuff, yourmom)
+				table.insert(result, attributes)
 			end
 		end
 	end
-	return newstuff
+	return result
 end
 
 for i,v in pairs(directory:GetDescendants()) do
    -- content pack database and inventory database don't contain information you need
    if v.ClassName == "ModuleScript" and v.Name ~= "ContentPackDB" and v.Name ~= "InventoryDB" then
-   		local success, error = pcall(function()
+   	local success, error = pcall(function()
    		writefile(v.Name .. ".json", hs:JSONEncode(moduleharvest(require(v))))
-   		end)
-   		if error then print(v.Name .. "had an error in it.") end
-   		wait(1) -- yields the script because my potato laptop will explode without this
-   		print(v.Name .. ": Completed.")
+   	end)
+   	if error then print(v.Name .. "had an error in it.") end
+   	wait(1) -- yields the script because my potato laptop will explode without this
+   	print(v.Name .. ": Completed.")
    end
 end
 
